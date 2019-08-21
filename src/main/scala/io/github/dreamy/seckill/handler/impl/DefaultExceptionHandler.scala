251ad527a93101eca2a44d2fd12c23801739b0b6
梦境迷离 <568845948@qq.com>
2019-08-21 21:55:10 +0800

    add handler exception

--- src/main/scala/io/github/dreamy/seckill/handler/impl/DefaultExceptionHandler.scala
+++ src/main/scala/io/github/dreamy/seckill/handler/impl/DefaultExceptionHandler.scala
@@ -1,7 +1,14 @@
 package io.github.dreamy.seckill.handler.impl
 
+import java.net.BindException
+import java.nio.ByteBuffer
+
+import io.github.dreamy.seckill.exception.GlobalException
 import io.github.dreamy.seckill.handler.ExceptionHandler
+import io.github.dreamy.seckill.presenter.{ CodeMsg, Result }
+import io.github.dreamy.seckill.serializer.GsonSerializerAdapter
 import io.undertow.server.HttpServerExchange
+import io.undertow.util.Headers
 
 /**
  * 默认的异常处理器
@@ -11,8 +18,28 @@ import io.undertow.server.HttpServerExchange
  * @version v1.0
  */
 class DefaultExceptionHandler extends ExceptionHandler {
+
+  private[this] lazy val contentType = "application/json;charset=utf-8"
+
   override def handleException(exchange: HttpServerExchange, cause: Throwable): Unit = {
     logger.error(s"found exception cause: ${cause.getMessage}")
+    cause match {
+      case ge: GlobalException =>
+        exchange.getResponseHeaders.put(Headers.CONTENT_TYPE, contentType)
+        exchange.setStatusCode(ge.getCm().getCode)
+        val jsonStr = GsonSerializerAdapter.getGson.toJson(Result.error(ge.getCm()))
+        exchange.getResponseSender.send(ByteBuffer.wrap(jsonStr.getBytes()))
+      case be: BindException =>
+        exchange.getResponseHeaders.put(Headers.CONTENT_TYPE, contentType)
+        exchange.setStatusCode(CodeMsg.BIND_ERROR.getCode)
+        val jsonStr = GsonSerializerAdapter.getGson.toJson(Result.error(CodeMsg.BIND_ERROR.fillArgs(be.getMessage)))
+        exchange.getResponseSender.send(ByteBuffer.wrap(jsonStr.getBytes()))
+      case _ =>
+        exchange.getResponseHeaders.put(Headers.CONTENT_TYPE, contentType)
+        exchange.setStatusCode(CodeMsg.INTERNAL_ERROR.getCode)
+        val jsonStr = GsonSerializerAdapter.getGson.toJson(Result.error(CodeMsg.SERVER_ERROR))
+        exchange.getResponseSender.send(ByteBuffer.wrap(jsonStr.getBytes()))
+    }
   }
 }
 
--- src/main/scala/io/github/dreamy/seckill/presenter/CodeMsg.scala
+++ src/main/scala/io/github/dreamy/seckill/presenter/CodeMsg.scala
@@ -1,5 +1,7 @@
 package io.github.dreamy.seckill.presenter
 
+import scala.util.Try
+
 /**
  * 状态码信息
  *
@@ -17,6 +19,9 @@ case class CodeMsg private(code: Int, msg: String) {
     val message = String.format(this.msg, args)
     new CodeMsg(code, message)
   }
+
+  //其他未定义异常默认500
+  def getCode = Try(this.code).getOrElse(500)
 }
 
 /**
@@ -33,77 +38,77 @@ object CodeMsg {
   /**
    * 服务端异常
    */
-  val SERVER_ERROR = CodeMsg(500100, "服务端异常")
+  val SERVER_ERROR = CodeMsg(510, "服务端异常")
 
   /**
    * 参数校验异常
    */
-  val BIND_ERROR = CodeMsg(500101, "参数校验异常：%s")
+  val BIND_ERROR = CodeMsg(511, "参数校验异常：%s")
 
   /**
    * 请求非法
    */
-  val REQUEST_ILLEGAL = CodeMsg(500102, "请求非法")
+  val REQUEST_ILLEGAL = CodeMsg(512, "请求非法")
 
   /**
    * 访问太频繁！
    */
-  val ACCESS_LIMIT_REACHED = CodeMsg(500104, "访问太频繁！")
+  val ACCESS_LIMIT_REACHED = CodeMsg(514, "访问太频繁！")
 
-  // 登录模块 5002XX
+  // 登录模块 52X
   /**
    * Session不存在或者已经失效
    */
-  val SESSION_ERROR = CodeMsg(500210, "Session不存在或者已经失效")
+  val SESSION_ERROR = CodeMsg(520, "Session不存在或者已经失效")
 
   /**
    * 登录密码不能为空
    */
-  val PASSWORD_EMPTY = CodeMsg(500211, "登录密码不能为空")
+  val PASSWORD_EMPTY = CodeMsg(521, "登录密码不能为空")
 
   /**
    * 手机号不能为空
    */
-  val MOBILE_EMPTY = CodeMsg(500212, "手机号不能为空")
+  val MOBILE_EMPTY = CodeMsg(522, "手机号不能为空")
 
   /**
    * 手机号格式错误
    */
-  val MOBILE_ERROR = CodeMsg(500213, "手机号格式错误")
+  val MOBILE_ERROR = CodeMsg(523, "手机号格式错误")
 
   /**
    * 手机号不存在
    */
-  val MOBILE_NOT_EXIST = CodeMsg(500214, "手机号不存在")
+  val MOBILE_NOT_EXIST = CodeMsg(524, "手机号不存在")
 
   /**
    * 密码错误
    */
-  val PASSWORD_ERROR = CodeMsg(500215, "密码错误")
+  val PASSWORD_ERROR = CodeMsg(525, "密码错误")
 
-  // 商品模块 5003XX
+  // 商品模块 53X
 
-  // 订单模块 5004XX
+  // 订单模块 54X
   /**
    * 订单不存在
    */
-  val ORDER_NOT_EXIST = CodeMsg(500400, "订单不存在")
+  val ORDER_NOT_EXIST = CodeMsg(540, "订单不存在")
 
   // 秒杀模块 5005XX
   /**
    * 商品已经秒杀完毕
    */
-  val SECKILL_OVER = CodeMsg(500500, "商品已经秒杀完毕")
+  val SECKILL_OVER = CodeMsg(550, "商品已经秒杀完毕")
 
   /**
    * 不能重复秒杀
    */
-  val REPEATE_SECKILL = CodeMsg(500501, "不能重复秒杀")
+  val REPEATE_SECKILL = CodeMsg(551, "不能重复秒杀")
 
   /**
    * 秒杀失败
    */
-  val SECKILL_FAIL = CodeMsg(500502, "秒杀失败")
+  val SECKILL_FAIL = CodeMsg(552, "秒杀失败")
 
 
   /**
